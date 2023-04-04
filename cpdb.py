07d1fa7e71c5e7890e11983bfad3a77c78d64434
emptybrother7 <emptybrother7@gmail.com>
2023-04-04 15:02:05 -0500

    trying to optimize

--- cpdb.py
+++ cpdb.py
@@ -1,13 +1,13 @@
 import os
 import discord
 from discord.ext import tasks, commands
-import requests
+import aiohttp
 import asyncio
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-COIN_ID = os.getenv('COIN_ID')  # Change this to the coin ID you want to track
+COIN_ID = os.getenv('COIN_ID')
 API_URL = f'https://api.coingecko.com/api/v3/simple/price?ids={COIN_ID}&vs_currencies=usd'
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
@@ -17,16 +17,23 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     await update_bot_status()
 
+async def fetch_price():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(API_URL) as response:
+            if response.status == 200:
+                return await response.json()
+            else:
+                print(f"Error fetching price for {COIN_ID}")
+                return None
+
 async def update_bot_status():
     while True:
-        response = requests.get(API_URL)
-        data = response.json()
-        if COIN_ID in data:
+        data = await fetch_price()
+        if data and COIN_ID in data:
             price = data[COIN_ID]['usd']
             status = f"{COIN_ID.capitalize()} ${price}"
             await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status))
-        else:
-            print(f"Error fetching price for {COIN_ID}")
-        await asyncio.sleep(60)  # Update interval (in seconds)
+        await asyncio.sleep(120)  # Update interval (in seconds)
 
 bot.run(TOKEN)
+
